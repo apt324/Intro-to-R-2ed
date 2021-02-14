f32be3ec960f952871fb0f4c742054b65ca9a58d
Austin Tarpey <apt324@lehigh.edu>
2021-02-14 17:25:46 -0500

    my name

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,5 @@
 # R script to accompany Intro to R
-# Copy for Austin Tarpey
+# Copy for Austin Paul Tarpey
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
